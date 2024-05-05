fcc93b18fdc60c5c92e2eafc14e50056b407aa76
impixel <57856090+JairSalinasA@users.noreply.github.com>
2024-05-05 14:42:33 -0600

    SE CORRIGE ERROR DE CLASE iUSUSARIO Repository.cs

---
+++ SmartPostSoftwareSol/DataAccess/Contracts/IGenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Contracts
+{
+    public interface IGenericRepository<Entity> where Entity : class
+    {
+        // Método para obtener todos los registros
+        IEnumerable<Entity> GetAll();
+
+        // Método para obtener un registro por ID
+        Entity GetById(int id);
+
+        // Método para añadir un nuevo registro
+        void Add(Entity entity);
+
+        // Método para actualizar un registro existente
+        void Update(Entity entity);
+
+        // Método para eliminar un registro
+        void Delete(Entity entity);
+    }
+}
+
+    
---
+++ SmartPostSoftwareSol/DataAccess/Contracts/IUserRepository.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Contracts
+{
+    public interface IUserRepository : IGenericRepository<Usuario>
+    {
+        public IEnumerable<Usuario> GetAll()
+        {
+
+        }
+
+        public Usuario GetById(int id)
+        {
+        }
+
+        public void Add(Usuario entity)
+        {
+
+        }
+
+        public void Update(Usuario entity)
+        {
+
+        }
+
+        public void Delete(Usuario entity)
+        {
+
+        }
+    }
+}
